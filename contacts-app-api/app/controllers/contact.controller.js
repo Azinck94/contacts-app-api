8b73d69803f2854ea79a9d9b59684badc1d97545
azinck94 <Andrew_zinck@student.uml.edu>
2018-05-04 10:36:24 -0400

    Added create function to controller -added create function with error handling to contact.controller.js

--- contacts-app-api/app/controllers/contact.controller.js
+++ contacts-app-api/app/controllers/contact.controller.js
@@ -2,7 +2,29 @@ const Contact = require('../models/contact.model.js');
 
 // Create and Save a new Contacts
 exports.create = (req, res) => {
+// Validates a request
+if(!req.body.content) {
+  return res.status(400).send({
+    message: "Contact content not filled"
+  });
+}
+// Create new Contact
+const contact = new Contact({
+  name: req.body.name,
+  number: req.body.name,
+  birthdate: req.body.name,
+  company: req.body.name
+});
 
+// Save Contact
+contact.save()
+.then(data => {
+  res.send(data);
+}).catch(err => {
+  res.status(500).send({
+    message: err.message || "Error occurred during contact creation"
+  });
+});
 };
 
 // Retrieve and return all contacts from the db
