30c2658ae7ae5cc5d5d59c136cc0609b96f147a5
azinck94 <Andrew_zinck@student.uml.edu>
2018-05-04 10:45:36 -0400

    Adds findOne and findAll functions to contact controllers -adds findOne function with error handling -adds findAll function with error handling

--- contacts-app-api/app/controllers/contact.controller.js
+++ contacts-app-api/app/controllers/contact.controller.js
@@ -29,12 +29,36 @@ contact.save()
 
 // Retrieve and return all contacts from the db
 exports.findAll = (req, res) => {
-
+  Contact.find()
+  .then(contacts => {
+    res.send(contacts);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || "Error occured while fetching contacts"
+    });
+  });
 };
 
 // Find a single contact with a contactId
 exports.findOne = (req, res) => {
-
+  Contact.findOne = (req.params.contactId)
+  .then(contact => {
+    if(!contact) {
+        return res.status(404).send({
+          message: "Unable to find contact " + req.params.contactId
+        });
+    }
+    res.send(contact);
+  }).catch(err => {
+      if(err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: "Unable to find contact " + req.params.contactId
+        });
+      }
+      return res.status(500).send({
+          message: "Error retrieving contact with id " + req.params.contactId
+      });
+  });
 };
 
 // Update a contact identified by the contactId in the request
