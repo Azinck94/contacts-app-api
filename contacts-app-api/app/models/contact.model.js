b1e5cec553d32613fc664b1ec448049084175b21
azinck94 <Andrew_zinck@student.uml.edu>
2018-05-05 18:13:23 -0400

    Updated Contact Contact
    - Adds personal, work to the phone number setter
    - Adds number, street, state, and zip to the address setter
    - Updated package.json

--- contacts-app-api/app/models/contact.model.js
+++ contacts-app-api/app/models/contact.model.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const ContactSchema = mongoose.Schema({
   name: String,
-  number: String,
-  birthdate: String,
+  number: {
+      work: String,
+      personal: String
+    },
+  address: {
+      number: String,
+      street: String,
+      state: String,
+      zip: String,
+    },
   company: String,
+  birthdate: String,
+  email: String,
 }, {
   timestamps: true
 });
